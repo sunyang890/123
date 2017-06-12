bcfb0d6cbd10366c9588be57d95d5710b70108f3
sunyang890 <sunyang890@163.com>
2017-06-12 22:14:33 +0800

    播放ROM内存部分的音乐

---
+++ webview/src/main/java/cn/edu/neusoft/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package cn.edu.neusoft.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_url)
+    TextView textUrl;
+    @BindView(R.id.button)
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.button)
+    public void onViewClicked() {
+    }
+}
